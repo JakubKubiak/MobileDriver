fef9336def32754e6a9020ded4184a6763163311
JakubKubiak <kruger666@gmail.com>
2017-08-09 12:43:18 +0200

    Build zmiana kamery dodane prefaby nowych aut zaczęta implementacja sterowania na virtualnym joyo oraz podmina starych skryptów na nowe!

--- MobileDriver/Assets/_Core/_Scripts/_Scripts2.0/DriverCresh.cs
+++ MobileDriver/Assets/_Core/_Scripts/_Scripts2.0/DriverCresh.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DriverCreash : MonoBehaviour
+public class DriverCresh : MonoBehaviour
 {
     public GameObject endText;
 
